102fbd022cc928594cbff598aaada5b940422b85
Aastha Shrivastava <aasthashrivastava410@gmail.com>
2019-07-22 15:18:35 +0530

    used set function to get unique values

---
+++ dict_sort.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+'''
+Author: Aastha Shrivastava
+PRN: 19030142001
+Date modified: 22nd JULY 2019
+
+Description: Program reads dictionary d which contains student id and room number pair
+and creates another dictionary room_ids which contains room number as keys which represent list of students staying in that room
+'''
+
+import time
+
+#declaring dictonary
+d={1:101,
+   2:101,
+   3:101,
+   4:102,
+   5:103,
+   6:103,
+   7:102}
+
+#for checking performance
+tym=time.time()
+
+#declaring empty dictionary
+room_ids={}
+
+for value in set(d.values()): #Here I have used set() to get the unique elements from list
+	room_ids[value]=[] #initializing each key to empty list
+
+#appending the id to the list
+for key in d.keys():
+	room_ids[d[key]].append(key)
+
+print(room_ids)
+print(time.time()-tym)
