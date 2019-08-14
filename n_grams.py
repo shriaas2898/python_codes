54d1f308123b94b56ce6012334607ae3a1e8aacf
Aastha Shrivastava <aasthashrivastava410@gmail.com>
2019-08-15 03:22:17 +0530

    Added exception handling

--- n_grams.py
+++ n_grams.py
@@ -3,18 +3,39 @@
 '''
 Author: Aastha Shrivastava
 PRN: 19030142001
-Date modified: 13th August 2019: Added documentation
+Date modified: 14th August 2019: Added exception handling
 Description: Program  reads the contents of file 'tempfile.txt', takes user input for number of grams and print the ngrams for the filr
 '''
 
-inp_file = open("tempfile.txt")
-n = int(input("Enter number grams"))
+try:
+    inp_file = open("tempfile.txt")
+    n = int(input("Enter number grams"))
 
-for line in inp_file:
-    count = 0
-    words = line.split()
-    for word in words[:-n+1]:
-        print(" ".join(words[count:count+n]))
-        count+=1
+    for line in inp_file:
+        count = 0
+        words = line.split()
+        for word in words[:-n + 1]:
+            print(" ".join(words[count:count + n]))
+            count += 1
 
-inp_file.close()
+
+
+except FileNotFoundError:
+    print("tempfile.txt not found")
+
+except PermissionError:
+    print("Permission denied to read/write file")
+
+except FileExistsError:
+    print("tempfile.txt already exists and cannot be overwritten")
+
+except IsADirectoryError:
+    print("Requested file is a directory")
+
+finally:
+    try:
+        inp_file.close()
+
+    except AttributeError:
+        pass
+
