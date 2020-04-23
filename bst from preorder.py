a5f545d5cab2b6e161ee78d26ea7cf87059fd1d9
Aastha Shrivastava <aasthashrivastava410@gmail.com>
2020-04-23 23:51:45 +0530

    Create bst from preorder.py
    Return the root node of a binary search tree that matches the given preorder traversal.

---
+++ bst from preorder.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        if len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        for i in preorder[1:]:
+            temp = root
+            added = False
+            while(not added):
+                if(i<temp.val):
+                    if(temp.left == None):
+                        newNode = TreeNode(i)
+                        temp.left = newNode
+                        added = True
+                        break
+                    temp = temp.left
+                    continue
+                    
+                else:
+                    if(temp.right == None):
+                        newNode = TreeNode(i)
+                        temp.right = newNode
+                        added = True
+                        break
+                    temp = temp.right
+                    continue
+        
+        return root            
+                    
+                
+    
+        
