584f4d0376f4c9d8c4f764485bcd5adb4219c9b0
Aastha Shrivastava <aasthashrivastava410@gmail.com>
2020-04-29 21:57:28 +0530

    Update LRU cache.py

--- LRU cache.py
+++ LRU cache.py
@@ -1,14 +1,32 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.keys = []
+        self.values = []
+        self.access = []
         self.capacity = capacity
-        self.cache = {}
-        self.count = 0
+        self.access_count = 0
+    
     def get(self, key: int) -> int:
-        if key in self.cache.keys():
-            self.cache[key] = count
-            count+=1
-            return self.cache.keys().index(key)
-        return -1
+        if key in self.keys:
+            self.access[self.keys.index(key)] = self.access_count
+            self.access_count+=1
+            return self.values[self.keys.index(key)]
+        else:
+            return -1
+    
     def put(self, key: int, value: int) -> None:
-                
+        if key not in self.keys:
+            if len(self.keys) == self.capacity:
+                idx = self.access.index(min(self.access))
+                self.keys.pop(idx)
+                self.values.pop(idx)
+                self.access.pop(idx)
+            self.keys.append(key)
+            self.values.append(value)
+            self.access.append(self.access_count)
+            
+        else:
+            self.values[self.keys.index(key)] = value
+            self.access[self.keys.index(key)] = self.access_count
+        self.access_count+=1    
